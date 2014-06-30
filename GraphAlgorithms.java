998ae92be0193e6f44df85264a752a30ace663fb
Thomas Stroeder <thomas.stroeder@email.de>
2014-06-30 17:09:15 +0000

    EA fixes

--- GraphAlgorithms.java
+++ GraphAlgorithms.java
@@ -9,6 +9,11 @@ import java.util.*;
 public abstract class GraphAlgorithms {
 
     /**
+     * Flag to enable special wishes of Erika for Dijkstra's algorithm.
+     */
+    private static final boolean ERIKA_MODE = true;
+    
+    /**
      * Prints exercise and solution for the Dijkstra Algorithm.
      * @param graph The graph.
      * @param start The start node.
@@ -29,59 +34,116 @@ public abstract class GraphAlgorithms {
             Collections.sort(nodes, comp);
         }
         final int size = nodes.size();
-        String[][] exTable = new String[size][size];
-        String[][] solTable = new String[size][size];
-        solTable[0][0] = "\\textbf{Knoten}";
-        exTable[0][0] = solTable[0][0];
-        Integer[] distances = new Integer[size];
-        Map<Node<N>, Integer> ids = new LinkedHashMap<Node<N>, Integer>();
-        int i = 1;
-        for (Node<N> node : nodes) {
-            if (!node.equals(start)) {
-                solTable[0][i] = "\\textbf{" + node.getLabel().toString() + "}";
-                exTable[0][i] = solTable[0][i];
-                ids.put(node, i);
-                i++;
-            }
-        }
-        int current = 0;
-        distances[current] = 0;
-        Set<Integer> used = new LinkedHashSet<Integer>();
-        for (i = 1; i < size; i++) {
-            used.add(current);
-            Node<N> currentNode = nodes.get(current);
-            solTable[i][0] = currentNode.getLabel().toString();
-            exTable[i][0] = "";
-            for (Pair<Integer, Node<N>> edge : graph.getAdjacencyList(currentNode)) {
-                Integer to = ids.get(edge.y);
-                if (to != null && (distances[to] == null || distances[to] > distances[current] + edge.x)) {
-                    distances[to] = distances[current] + edge.x;
+        final String[][] exTable;
+        final String[][] solTable;
+        if (GraphAlgorithms.ERIKA_MODE) {
+            exTable = new String[size][size + 1];
+            solTable = new String[size][size + 1];
+            solTable[0][0] = "\\texttt{v}";
+            exTable[0][0] = solTable[0][0];
+            Integer[] distances = new Integer[size];
+            Map<Node<N>, Integer> ids = new LinkedHashMap<Node<N>, Integer>();
+            int i = 1;
+            for (Node<N> node : nodes) {
+                if (!node.equals(start)) {
+                    solTable[0][i + 1] = "\\texttt{key[}" + node.getLabel().toString() + "\\texttt{]}";
+                    exTable[0][i + 1] = solTable[0][i + 1];
+                    ids.put(node, i);
+                    i++;
                 }
             }
-            Integer curMin = null;
-            int minIndex = -1;
-            for (int j = 1; j < size; j++) {
-                final Integer dist = distances[j];
-                if (dist == null) {
-                    solTable[i][j] = "$\\infty$";
-                } else {
-                    if (used.contains(j)) {
-                        solTable[i][j] = "\\textbf{--}";
+            solTable[0][1] = "\\texttt{key[}" + start.getLabel().toString() + "\\texttt{]}";
+            exTable[0][1] = solTable[0][1];
+            int current = 0;
+            distances[current] = 0;
+            Set<Integer> used = new LinkedHashSet<Integer>();
+            for (i = 1; i < size; i++) {
+                used.add(current);
+                Node<N> currentNode = nodes.get(current);
+                solTable[i][0] = currentNode.getLabel().toString();
+                exTable[i][0] = "";
+                for (Pair<Integer, Node<N>> edge : graph.getAdjacencyList(currentNode)) {
+                    Integer to = ids.get(edge.y);
+                    if (to != null && (distances[to] == null || distances[to] > distances[current] + edge.x)) {
+                        distances[to] = distances[current] + edge.x;
+                    }
+                }
+                Integer curMin = null;
+                int minIndex = -1;
+                for (int j = 1; j <= size; j++) {
+                    final Integer dist = distances[j - 1];
+                    if (dist == null) {
+                        solTable[i][j] = "$\\infty$";
                     } else {
-                        if (curMin == null || curMin > dist) {
+                        if (!used.contains(j - 1) && (curMin == null || curMin > dist)) {
                             curMin = dist;
-                            minIndex = j;
+                            minIndex = j - 1;
                         }
                         solTable[i][j] = "" + dist;
                     }
+                    exTable[i][j] = "";
                 }
-                exTable[i][j] = "";
+                if (minIndex < 0) {
+                    // no shortening possible
+                    break;
+                }
+                current = minIndex;
             }
-            if (minIndex < 0) {
-                // no shortening possible
-                break;
+        } else {
+            exTable = new String[size][size];
+            solTable = new String[size][size];
+            solTable[0][0] = "\\textbf{Knoten}";
+            exTable[0][0] = solTable[0][0];
+            Integer[] distances = new Integer[size];
+            Map<Node<N>, Integer> ids = new LinkedHashMap<Node<N>, Integer>();
+            int i = 1;
+            for (Node<N> node : nodes) {
+                if (!node.equals(start)) {
+                    solTable[0][i] = "\\textbf{" + node.getLabel().toString() + "}";
+                    exTable[0][i] = solTable[0][i];
+                    ids.put(node, i);
+                    i++;
+                }
             }
-            current = minIndex;
+            int current = 0;
+            distances[current] = 0;
+            Set<Integer> used = new LinkedHashSet<Integer>();
+            for (i = 1; i < size; i++) {
+                used.add(current);
+                Node<N> currentNode = nodes.get(current);
+                solTable[i][0] = currentNode.getLabel().toString();
+                exTable[i][0] = "";
+                for (Pair<Integer, Node<N>> edge : graph.getAdjacencyList(currentNode)) {
+                    Integer to = ids.get(edge.y);
+                    if (to != null && (distances[to] == null || distances[to] > distances[current] + edge.x)) {
+                        distances[to] = distances[current] + edge.x;
+                    }
+                }
+                Integer curMin = null;
+                int minIndex = -1;
+                for (int j = 1; j < size; j++) {
+                    final Integer dist = distances[j];
+                    if (dist == null) {
+                        solTable[i][j] = "$\\infty$";
+                    } else {
+                        if (used.contains(j)) {
+                            solTable[i][j] = "\\textbf{--}";
+                        } else {
+                            if (curMin == null || curMin > dist) {
+                                curMin = dist;
+                                minIndex = j;
+                            }
+                            solTable[i][j] = "" + dist;
+                        }
+                    }
+                    exTable[i][j] = "";
+                }
+                if (minIndex < 0) {
+                    // no shortening possible
+                    break;
+                }
+                current = minIndex;
+            }
         }
         exTable[1][0] = start.getLabel().toString();
         exWriter.write("Betrachten Sie den folgenden Graphen:\\\\[2ex]");
@@ -93,7 +155,13 @@ public abstract class GraphAlgorithms {
         exWriter.newLine();
         exWriter.write("F\\\"uhren Sie den Dijkstra Algorithmus auf diesem Graphen mit dem Startknoten ");
         exWriter.write(start.getLabel().toString());
-        exWriter.write(" aus. F\\\"ullen Sie dazu die nachfolgende Tabelle aus:\\\\[2ex]");
+        if (GraphAlgorithms.ERIKA_MODE) {
+            exWriter.write(" aus. F\\\"ullen Sie dazu die nachfolgende Tabelle aus, indem Sie den Wert von ");
+            exWriter.write("\\texttt{v} und \\texttt{key} nach jeder Iteration der \\texttt{while}-Schleife ");
+            exWriter.write("eintragen:\\\\[2ex]");
+        } else {
+            exWriter.write(" aus. F\\\"ullen Sie dazu die nachfolgende Tabelle aus:\\\\[2ex]");
+        }
         exWriter.newLine();
         TikZUtils.printBeginning(TikZUtils.CENTER, exWriter);
         exWriter.newLine();
