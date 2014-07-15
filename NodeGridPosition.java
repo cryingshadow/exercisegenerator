cfcc75fcb23f7fc9b525758e5cfcc9bc3a30c2be
Thomas Stroeder <thomas.stroeder@email.de>
2014-07-15 08:25:04 +0000

    this class was missing

---
+++ NodeGridPosition.java
@@ -0,0 +1,229 @@
+import java.util.*;
+
+/**
+ * Represents a node position in a grid layout.
+ * @author Thomas Ströder
+ * @version 1.0
+ */
+public class NodeGridPosition {
+
+    /**
+     * The position east of the current one.
+     */
+    public NodeGridPosition east;
+
+    /**
+     * Does this position allow diagonal edges?
+     */
+    public final boolean hasDiagonals;
+
+    /**
+     * The position north of the current one.
+     */
+    public NodeGridPosition north;
+
+    /**
+     * The position northeast of the current one.
+     */
+    public NodeGridPosition northeast;
+
+    /**
+     * The position northwest of the current one.
+     */
+    public NodeGridPosition northwest;
+
+    /**
+     * The position south of the current one.
+     */
+    public NodeGridPosition south;
+
+    /**
+     * The position southeast of the current one.
+     */
+    public NodeGridPosition southeast;
+
+    /**
+     * The position southwest of the current one.
+     */
+    public NodeGridPosition southwest;
+
+    /**
+     * The position west of the current one.
+     */
+    public NodeGridPosition west;
+
+    /**
+     * The x coordinate of this position.
+     */
+    public final int x;
+
+    /**
+     * The y coordinate of this position.
+     */
+    public final int y;
+
+    /**
+     * @param xPos The x coordinate of this position.
+     * @param yPos The y coordinate of this position.
+     * @param withDiagonals Does this position allow diagonal edges?
+     */
+    public NodeGridPosition(int xPos, int yPos, boolean withDiagonals) {
+        this.x = xPos;
+        this.y = yPos;
+        this.hasDiagonals = withDiagonals;
+    }
+
+    /**
+     * @return A list of all existing positions next to the current one.
+     */
+    public List<Pair<Pair<Integer, Integer>, Boolean>> getExistingPositions() {
+        List<Pair<Pair<Integer, Integer>, Boolean>> res = new ArrayList<Pair<Pair<Integer, Integer>, Boolean>>();
+        if (this.north != null) {
+            res.add(
+                new Pair<Pair<Integer, Integer>, Boolean>(
+                    new Pair<Integer, Integer>(this.x, this.y - 1),
+                    !this.hasDiagonals
+                )
+            );
+        }
+        if (this.east != null) {
+            res.add(
+                new Pair<Pair<Integer, Integer>, Boolean>(
+                    new Pair<Integer, Integer>(this.x + 1, this.y),
+                    !this.hasDiagonals
+                )
+            );
+        }
+        if (this.south != null) {
+            res.add(
+                new Pair<Pair<Integer, Integer>, Boolean>(
+                    new Pair<Integer, Integer>(this.x, this.y + 1),
+                    !this.hasDiagonals
+                )
+            );
+        }
+        if (this.west != null) {
+            res.add(
+                new Pair<Pair<Integer, Integer>, Boolean>(
+                    new Pair<Integer, Integer>(this.x - 1, this.y),
+                    !this.hasDiagonals
+                )
+            );
+        }
+        if (this.hasDiagonals) {
+            if (this.northeast != null) {
+                res.add(
+                    new Pair<Pair<Integer, Integer>, Boolean>(new Pair<Integer, Integer>(this.x + 1, this.y - 1), true)
+                );
+            }
+            if (this.southeast != null) {
+                res.add(
+                    new Pair<Pair<Integer, Integer>, Boolean>(new Pair<Integer, Integer>(this.x + 1, this.y + 1), true)
+                );
+            }
+            if (this.southwest != null) {
+                res.add(
+                    new Pair<Pair<Integer, Integer>, Boolean>(new Pair<Integer, Integer>(this.x - 1, this.y + 1), true)
+                );
+            }
+            if (this.northwest != null) {
+                res.add(
+                    new Pair<Pair<Integer, Integer>, Boolean>(new Pair<Integer, Integer>(this.x - 1, this.y - 1), true)
+                );
+            }
+        }
+        return res;
+    }
+
+    /**
+     * @return A list of all free positions next to the current one.
+     */
+    public List<Pair<Pair<Integer, Integer>, Boolean>> getFreePositions() {
+        List<Pair<Pair<Integer, Integer>, Boolean>> res = new ArrayList<Pair<Pair<Integer, Integer>, Boolean>>();
+        if (this.north == null) {
+            res.add(
+                new Pair<Pair<Integer, Integer>, Boolean>(
+                    new Pair<Integer, Integer>(this.x, this.y - 1),
+                    !this.hasDiagonals
+                )
+            );
+        }
+        if (this.east == null) {
+            res.add(
+                new Pair<Pair<Integer, Integer>, Boolean>(
+                    new Pair<Integer, Integer>(this.x + 1, this.y),
+                    !this.hasDiagonals
+                )
+            );
+        }
+        if (this.south == null) {
+            res.add(
+                new Pair<Pair<Integer, Integer>, Boolean>(
+                    new Pair<Integer, Integer>(this.x, this.y + 1),
+                    !this.hasDiagonals
+                )
+            );
+        }
+        if (this.west == null) {
+            res.add(
+                new Pair<Pair<Integer, Integer>, Boolean>(
+                    new Pair<Integer, Integer>(this.x - 1, this.y),
+                    !this.hasDiagonals
+                )
+            );
+        }
+        if (this.hasDiagonals) {
+            if (this.northeast == null) {
+                res.add(
+                    new Pair<Pair<Integer, Integer>, Boolean>(new Pair<Integer, Integer>(this.x + 1, this.y - 1), true)
+                );
+            }
+            if (this.southeast == null) {
+                res.add(
+                    new Pair<Pair<Integer, Integer>, Boolean>(new Pair<Integer, Integer>(this.x + 1, this.y + 1), true)
+                );
+            }
+            if (this.southwest == null) {
+                res.add(
+                    new Pair<Pair<Integer, Integer>, Boolean>(new Pair<Integer, Integer>(this.x - 1, this.y + 1), true)
+                );
+            }
+            if (this.northwest == null) {
+                res.add(
+                    new Pair<Pair<Integer, Integer>, Boolean>(new Pair<Integer, Integer>(this.x - 1, this.y - 1), true)
+                );
+            }
+        }
+        return res;
+    }
+
+    /**
+     * @return True if there is at least one free position next to the current one.
+     */
+    public boolean hasFreePosition() {
+        return
+            this.north == null
+            || this.east == null
+            || this.south == null
+            || this.west == null
+            || (
+                this.hasDiagonals
+                && (
+                    this.northeast == null
+                    || this.southeast == null
+                    || this.southwest == null
+                    || this.northwest == null
+                )
+            );
+    }
+
+    /**
+     * @param gen A random number generator.
+     * @return A random free position next to the current position.
+     */
+    public Pair<Pair<Integer, Integer>, Boolean> randomFreePosition(Random gen) {
+        List<Pair<Pair<Integer, Integer>, Boolean>> res = this.getFreePositions();
+        return res.get(gen.nextInt(res.size()));
+    }
+
+}
