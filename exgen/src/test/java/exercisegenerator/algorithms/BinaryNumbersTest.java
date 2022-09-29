1c4be0813637ff75a5ab64030fc8379cf4f34840
Thomas Ströder <thomas.stroeder@email.de>
2022-09-29 08:52:36 +0100

    refactor binary numbers tests

--- exgen/src/test/java/exercisegenerator/algorithms/BinaryNumbersTest.java
+++ exgen/src/test/java/exercisegenerator/algorithms/BinaryNumbersTest.java
@@ -4,63 +4,85 @@ import org.testng.*;
 import org.testng.annotations.*;
 
 import exercisegenerator.io.*;
-import exercisegenerator.structures.*;
 import exercisegenerator.structures.binary.*;
 
 public class BinaryNumbersTest {
 
-    @Test
-    public void fromFloat() {
-        Assert.assertEquals(BinaryNumbers.fromFloat(BitString.parse("00110110"), 3, 4), "1,375");
-        Assert.assertEquals(
-            BinaryNumbers.fromFloat(BitString.parse("01000001011011000000000000000000"), 8, 23),
-            "14,75"
-        );
-        Assert.assertEquals(BinaryNumbers.fromFloat(BitString.parse("10010000"), 3, 4), "-0,25");
-        Assert.assertEquals(BinaryNumbers.fromFloat(BitString.parse("00000000"), 3, 4), "0,0");
-        Assert.assertEquals(BinaryNumbers.fromFloat(BitString.parse("10000000"), 3, 4), "-0,0");
-        Assert.assertEquals(BinaryNumbers.fromFloat(BitString.parse("00010000"), 3, 4), "0,25");
-        Assert.assertEquals(BinaryNumbers.fromFloat(BitString.parse("01110000"), 3, 4), "inf");
-        Assert.assertEquals(BinaryNumbers.fromFloat(BitString.parse("11110000"), 3, 4), "-inf");
-        Assert.assertEquals(BinaryNumbers.fromFloat(BitString.parse("11111111"), 3, 4), "NaN");
-        Assert.assertEquals(BinaryNumbers.fromFloat(BitString.parse("00001000"), 3, 4), "0,125");
-        Assert.assertEquals(BinaryNumbers.fromFloat(BitString.parse("00000100"), 3, 4), "0,0625");
-        Assert.assertEquals(BinaryNumbers.fromFloat(BitString.parse("00000010"), 3, 4), "0,03125");
-        Assert.assertEquals(BinaryNumbers.fromFloat(BitString.parse("00000001"), 3, 4), "0,015625");
-        Assert.assertEquals(BinaryNumbers.fromFloat(BitString.parse("01101111"), 3, 4), "15,5");
+    @Test(dataProvider="fromFloatData")
+    public void fromFloat(
+        final String bits,
+        final int exponentLength,
+        final int mantissaLength,
+        final String expected
+    ) {
+        Assert.assertEquals(BinaryNumbers.fromFloat(BitString.parse(bits), exponentLength, mantissaLength), expected);
     }
 
-    @Test
-    public void fromOnesComplement() {
-        Assert.assertEquals(BinaryNumbers.fromOnesComplement(BitString.parse("0011")), 3);
-        Assert.assertEquals(BinaryNumbers.fromOnesComplement(BitString.parse("0100")), 4);
-        Assert.assertEquals(BinaryNumbers.fromOnesComplement(BitString.parse("00")), 0);
-        Assert.assertEquals(BinaryNumbers.fromOnesComplement(BitString.parse("000000000")), 0);
-        Assert.assertEquals(BinaryNumbers.fromOnesComplement(BitString.parse("00000000001")), 1);
-        Assert.assertEquals(BinaryNumbers.fromOnesComplement(BitString.parse("0111")), 7);
-        Assert.assertEquals(BinaryNumbers.fromOnesComplement(BitString.parse("1010")), -5);
-        Assert.assertEquals(BinaryNumbers.fromOnesComplement(BitString.parse("1000")), -7);
-        Assert.assertEquals(BinaryNumbers.fromOnesComplement(BitString.parse("1111011")), -4);
-        Assert.assertEquals(BinaryNumbers.fromOnesComplement(BitString.parse("110")), -1);
-        Assert.assertEquals(BinaryNumbers.fromOnesComplement(BitString.parse("1")), 0);
-        Assert.assertEquals(BinaryNumbers.fromOnesComplement(BitString.parse("0")), 0);
+    @DataProvider
+    public Object[][] fromFloatData() {
+        return new Object[][] {
+            {"00110110", 3, 4, "1,375"},
+            {"01000001011011000000000000000000", 8, 23, "14,75"},
+            {"10010000", 3, 4, "-0,25"},
+            {"00000000", 3, 4, "0,0"},
+            {"10000000", 3, 4, "-0,0"},
+            {"00010000", 3, 4, "0,25"},
+            {"01110000", 3, 4, "inf"},
+            {"11110000", 3, 4, "-inf"},
+            {"11111111", 3, 4, "NaN"},
+            {"00001000", 3, 4, "0,125"},
+            {"00000100", 3, 4, "0,0625"},
+            {"00000010", 3, 4, "0,03125"},
+            {"00000001", 3, 4, "0,015625"},
+            {"01101111", 3, 4, "15,5"}
+        };
     }
 
-    @Test
-    public void fromTwosComplement() {
-        Assert.assertEquals(BinaryNumbers.fromTwosComplement(BitString.parse("0011")), 3);
-        Assert.assertEquals(BinaryNumbers.fromTwosComplement(BitString.parse("0100")), 4);
-        Assert.assertEquals(BinaryNumbers.fromTwosComplement(BitString.parse("00")), 0);
-        Assert.assertEquals(BinaryNumbers.fromTwosComplement(BitString.parse("000000000")), 0);
-        Assert.assertEquals(BinaryNumbers.fromTwosComplement(BitString.parse("00000000001")), 1);
-        Assert.assertEquals(BinaryNumbers.fromTwosComplement(BitString.parse("0111")), 7);
-        Assert.assertEquals(BinaryNumbers.fromTwosComplement(BitString.parse("1011")), -5);
-        Assert.assertEquals(BinaryNumbers.fromTwosComplement(BitString.parse("1001")), -7);
-        Assert.assertEquals(BinaryNumbers.fromTwosComplement(BitString.parse("1111100")), -4);
-        Assert.assertEquals(BinaryNumbers.fromTwosComplement(BitString.parse("111")), -1);
-        Assert.assertEquals(BinaryNumbers.fromTwosComplement(BitString.parse("1000")), -8);
-        Assert.assertEquals(BinaryNumbers.fromTwosComplement(BitString.parse("1")), -1);
-        Assert.assertEquals(BinaryNumbers.fromTwosComplement(BitString.parse("0")), 0);
+    @Test(dataProvider="fromOnesData")
+    public void fromOnesComplement(final String bits, final int expected) {
+        Assert.assertEquals(BinaryNumbers.fromOnesComplement(BitString.parse(bits)), expected);
+    }
+
+    @DataProvider
+    public Object[][] fromOnesData() {
+        return new Object[][] {
+            {"0011", 3},
+            {"0100", 4},
+            {"00", 0},
+            {"000000000", 0},
+            {"00000000001", 1},
+            {"0111", 7},
+            {"1010", -5},
+            {"1000", -7},
+            {"1111011", -4},
+            {"110", -1},
+            {"1", 0},
+            {"0", 0}
+        };
+    }
+
+    @Test(dataProvider="fromTwosData")
+    public void fromTwosComplement(final String bits, final int expected) {
+        Assert.assertEquals(BinaryNumbers.fromTwosComplement(BitString.parse(bits)), expected);
+    }
+
+    @DataProvider
+    public Object[][] fromTwosData() {
+        return new Object[][] {
+            {"0011", 3},
+            {"0100", 4},
+            {"00", 0},
+            {"000000000", 0},
+            {"00000000001", 1},
+            {"0111", 7},
+            {"1011", -5},
+            {"1001", -7},
+            {"1111100", -4},
+            {"111", -1},
+            {"1000", -8},
+            {"1", -1},
+            {"0", 0}
+        };
     }
 
     @BeforeMethod
@@ -68,62 +90,105 @@ public class BinaryNumbersTest {
         LaTeXUtils.reset();
     }
 
-    @Test
-    public void toFloat() {
-        Assert.assertEquals(BinaryNumbers.toFloat("1,4", 3, 4).toString(), "00110110");
-        Assert.assertEquals(BinaryNumbers.toFloat("18,4", 8, 23).toString(), "01000001100100110011001100110011");
-        Assert.assertEquals(BinaryNumbers.toFloat("-0,25", 3, 4).toString(), "10010000");
-        Assert.assertEquals(BinaryNumbers.toFloat("0", 3, 4).toString(), "00000000");
-        Assert.assertEquals(BinaryNumbers.toFloat("-0", 3, 4).toString(), "10000000");
-        Assert.assertEquals(BinaryNumbers.toFloat("-0,1", 3, 4).toString(), "10000110");
-        Assert.assertEquals(BinaryNumbers.toFloat("0,0", 3, 4).toString(), "00000000");
-        Assert.assertEquals(BinaryNumbers.toFloat("0,00", 3, 4).toString(), "00000000");
-        Assert.assertEquals(BinaryNumbers.toFloat("-00,0", 3, 4).toString(), "10000000");
-        Assert.assertEquals(BinaryNumbers.toFloat("0,2500", 3, 4).toString(), "00010000");
-        Assert.assertEquals(BinaryNumbers.toFloat("0,0625", 5, 10).toString(), "0010110000000000");
-        Assert.assertEquals(BinaryNumbers.toFloat("0,125", 3, 4).toString(), "00001000");
-        Assert.assertEquals(BinaryNumbers.toFloat("0,0625", 3, 4).toString(), "00000100");
-        Assert.assertEquals(BinaryNumbers.toFloat("0,015625", 3, 4).toString(), "00000001");
-        Assert.assertEquals(BinaryNumbers.toFloat("0,01", 3, 4).toString(), "00000000");
+    @Test(dataProvider="toFloatData")
+    public void toFloat(final String number, final int exponentLength, final int mantissaLength, final String expected) {
+        Assert.assertEquals(BinaryNumbers.toFloat(number, exponentLength, mantissaLength).toString(), expected);
     }
 
-    @Test
-    public void toOnesComplement() {
-        Assert.assertEquals(BinaryNumbers.toOnesComplement(3, 4).toString(), "0011");
-        Assert.assertEquals(BinaryNumbers.toOnesComplement(4, 4).toString(), "0100");
-        Assert.assertEquals(BinaryNumbers.toOnesComplement(0, 2).toString(), "00");
-        Assert.assertEquals(BinaryNumbers.toOnesComplement(0, 9).toString(), "000000000");
-        Assert.assertEquals(BinaryNumbers.toOnesComplement(1, 11).toString(), "00000000001");
-        Assert.assertEquals(BinaryNumbers.toOnesComplement(7, 4).toString(), "0111");
-        Assert.assertEquals(BinaryNumbers.toOnesComplement(-5, 4).toString(), "1010");
-        Assert.assertEquals(BinaryNumbers.toOnesComplement(-7, 4).toString(), "1000");
-        Assert.assertEquals(BinaryNumbers.toOnesComplement(-4, 7).toString(), "1111011");
-        Assert.assertEquals(BinaryNumbers.toOnesComplement(-1, 3).toString(), "110");
-        Assert.assertThrows(() -> BinaryNumbers.toOnesComplement(13, 4));
-        Assert.assertThrows(() -> BinaryNumbers.toOnesComplement(8, 4));
-        Assert.assertThrows(() -> BinaryNumbers.toOnesComplement(-8, 4));
-        Assert.assertThrows(() -> BinaryNumbers.toOnesComplement(128, 8));
-        Assert.assertThrows(() -> BinaryNumbers.toOnesComplement(-128, 8));
+    @DataProvider
+    public Object[][] toFloatData() {
+        return new Object[][] {
+            {"1,4", 3, 4, "00110110"},
+            {"18,4", 8, 23, "01000001100100110011001100110011"},
+            {"-0,25", 3, 4, "10010000"},
+            {"0", 3, 4, "00000000"},
+            {"-0", 3, 4, "10000000"},
+            {"-0,1", 3, 4, "10000110"},
+            {"0,0", 3, 4, "00000000"},
+            {"0,00", 3, 4, "00000000"},
+            {"-00,0", 3, 4, "10000000"},
+            {"0,2500", 3, 4, "00010000"},
+            {"0,0625", 5, 10, "0010110000000000"},
+            {"0,125", 3, 4, "00001000"},
+            {"0,0625", 3, 4, "00000100"},
+            {"0,015625", 3, 4, "00000001"},
+            {"0,01", 3, 4, "00000000"}
+        };
     }
 
-    @Test
-    public void toTwosComplement() {
-        Assert.assertEquals(BinaryNumbers.toTwosComplement(3, 4).toString(), "0011");
-        Assert.assertEquals(BinaryNumbers.toTwosComplement(4, 4).toString(), "0100");
-        Assert.assertEquals(BinaryNumbers.toTwosComplement(0, 2).toString(), "00");
-        Assert.assertEquals(BinaryNumbers.toTwosComplement(0, 9).toString(), "000000000");
-        Assert.assertEquals(BinaryNumbers.toTwosComplement(1, 11).toString(), "00000000001");
-        Assert.assertEquals(BinaryNumbers.toTwosComplement(7, 4).toString(), "0111");
-        Assert.assertEquals(BinaryNumbers.toTwosComplement(-5, 4).toString(), "1011");
-        Assert.assertEquals(BinaryNumbers.toTwosComplement(-7, 4).toString(), "1001");
-        Assert.assertEquals(BinaryNumbers.toTwosComplement(-4, 7).toString(), "1111100");
-        Assert.assertEquals(BinaryNumbers.toTwosComplement(-1, 3).toString(), "111");
-        Assert.assertEquals(BinaryNumbers.toTwosComplement(-8, 4).toString(), "1000");
-        Assert.assertThrows(() -> BinaryNumbers.toTwosComplement(7, 3));
-        Assert.assertThrows(() -> BinaryNumbers.toTwosComplement(8, 4));
-        Assert.assertThrows(() -> BinaryNumbers.toTwosComplement(-9, 4));
-        Assert.assertThrows(() -> BinaryNumbers.toTwosComplement(128, 8));
-        Assert.assertThrows(() -> BinaryNumbers.toTwosComplement(-129, 8));
+    @Test(dataProvider="toOnesData")
+    public void toOnesComplement(final int number, final int bitLength, final String expected) {
+        Assert.assertEquals(BinaryNumbers.toOnesComplement(number, bitLength).toString(), expected);
+    }
+
+    @Test(dataProvider="toOnesErrorData")
+    public void toOnesComplementErrors(final int number, final int bitLength) {
+        Assert.assertThrows(() -> BinaryNumbers.toOnesComplement(number, bitLength));
+    }
+
+    @DataProvider
+    public Object[][] toOnesData() {
+        return new Object[][] {
+            {3, 4, "0011"},
+            {4, 4, "0100"},
+            {0, 2, "00"},
+            {0, 9, "000000000"},
+            {1, 11, "00000000001"},
+            {7, 4, "0111"},
+            {-5, 4, "1010"},
+            {-7, 4, "1000"},
+            {-4, 7, "1111011"},
+            {-1, 3, "110"}
+        };
+    }
+
+    @DataProvider
+    public Object[][] toOnesErrorData() {
+        return new Object[][] {
+            {13, 4},
+            {8, 4},
+            {-8, 4},
+            {128, 8},
+            {-128, 8}
+        };
+    }
+
+    @Test(dataProvider="toTwosData")
+    public void toTwosComplement(final int number, final int bitLength, final String expected) {
+        Assert.assertEquals(BinaryNumbers.toTwosComplement(number, bitLength).toString(), expected);
+    }
+
+    @Test(dataProvider="toTwosErrorData")
+    public void toTwosComplementErrors(final int number, final int bitLength) {
+        Assert.assertThrows(() -> BinaryNumbers.toTwosComplement(number, bitLength));
+    }
+
+    @DataProvider
+    public Object[][] toTwosData() {
+        return new Object[][] {
+            {3, 4, "0011"},
+            {4, 4, "0100"},
+            {0, 2, "00"},
+            {0, 9, "000000000"},
+            {1, 11, "00000000001"},
+            {7, 4, "0111"},
+            {-5, 4, "1011"},
+            {-7, 4, "1001"},
+            {-4, 7, "1111100"},
+            {-1, 3, "111"},
+            {-8, 4, "1000"}
+        };
+    }
+
+    @DataProvider
+    public Object[][] toTwosErrorData() {
+        return new Object[][] {
+            {7, 3},
+            {8, 4},
+            {-9, 4},
+            {128, 8},
+            {-129, 8}
+        };
     }
 
 }
